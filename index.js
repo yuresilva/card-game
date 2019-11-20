7c484120d1f021ad1748b2ae38fc9ba2f728b314
yuresilva <nogueiranetoy@gmail.com>
2019-11-20 18:30:45 -0300

    feat: test

--- index.js
+++ index.js
@@ -1,21 +1,39 @@
-const cartas = ["az", 2, 3, 4, 5, 6, 7, 8, 9, 10, "j", "q", "k"];
+const readlineSync = require("readline-sync");
 
-const readline = require("readline-sync");
+const cartas = ["az :1 ", 2, 3, 4, 5, 6, 7, 8, 9, 10, "j: 11", "q: 12", "k:13"];
 
-const usuario = {
-  carteira: 10000
-};
+let carteira = 10000;
+let valorAposta = 0;
+let resultadoAposta = 0;
 
-var rand = cartas[Math.floor(Math.random() * cartas.length)];
-console.log(rand);
+let userName = readlineSync.question("Qual seu nome? ");
 
-function start() {
-  const content = {};
-  content.usuario = nomeUsuario();
-  function nomeUsuario() {
-    return readline.question("digite seu nome: ");
-  }
-  console.log(content);
+console.log(userName);
+
+function shuffleCards(cards) {
+  return cards[Math.floor(Math.random() * cards.length)];
 }
+shuffleCards(cartas);
 
-start();
+function round(cartas, cartaAnterior) {
+  let cardsShuffled = shuffleCards(cartas); // a cada rodada voce precisa embaralhar as cartas
+  const card = cardsShuffled[0];
+  let perguntaAposta = readlineSync.question(
+    "Qual carta vai sair valor alto ou baixa? "
+  );
+  console.log(perguntaAposta);
+
+  valorAposta = parseFloat(
+    readlineSync.question("Quanto vai querer apostar? ")
+  );
+  console.log(valorAposta);
+
+  let cardsShuffled2 = shuffleCards(cartaAnterior);
+  const cartaAnterior = cardsShuffled2[0];
+
+  if (card > cartaAnterior) {
+    resultadoAposta = carteira + valorAposta;
+  } else {
+    resultadoAposta = carteira - valorAposta;
+  }
+}
